59584b986eb663f40dd84f83d47d89c3beb22429
cafesit8 <leliasgomer@gmail.com>
2024-07-24 16:06:52 -0500

    create the modal for playlist

--- src/components/CardMusic.vue
+++ src/components/CardMusic.vue
@@ -1,8 +1,19 @@
 <template>
-  <article class="card" @click="handleClick">
+  <article class="card">
+    <q-btn-dropdown color="" dropdown-icon="more_vert" unelevated @click="playListClick">
+      <div v-show="playList.length !== 0">
+        <q-list>
+          <q-item v-for="play in playList" :key="play.id" clickable v-close-popup>
+            <q-item-section>
+              <q-item-label>{{ play.name }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+    </q-btn-dropdown>
     <picture>
       <img :src="album?.album.cover_medium" alt="">
-      <svg width="40" height="40" viewBox="0 0 24 24" fill="currentColor"
+      <svg @click="handleClick" width="40" height="40" viewBox="0 0 24 24" fill="currentColor"
         class="icon icon-tabler icons-tabler-filled icon-tabler-player-play">
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" />
@@ -17,6 +28,8 @@
 <script>
 import { defineComponent } from 'vue'
 import { useMusic } from 'src/composables/useMusic'
+import { useDialog } from 'src/composables/useDialog'
+import { usePlayList } from 'src/composables/usePlayList'
 
 export default defineComponent({
   name: 'CardMusic',
@@ -26,11 +39,12 @@ export default defineComponent({
     }
   },
   setup (props) {
+    const { toggleDialog } = useDialog()
+    const { playList } = usePlayList()
     const { playSong, setSongInfo } = useMusic()
     const { title, artist: { name: singer }, preview: song, album: { cover_medium: cover }, id, duration } = props.album
 
     function handleClick () {
-      // if (songInfo.value.id === id) return
       playSong()
       setSongInfo({
         id,
@@ -38,11 +52,20 @@ export default defineComponent({
         singer,
         cover,
         song,
-        duration
+        duration,
+        toggleDialog
       })
     }
+
+    function playListClick () {
+      if (playList.value.length === 0) {
+        toggleDialog()
+      }
+    }
     return {
-      handleClick
+      handleClick,
+      playListClick,
+      playList
     }
   }
 })
@@ -57,10 +80,30 @@ export default defineComponent({
   flex-direction: column;
   transition: .3s;
   cursor: pointer;
+  position: relative;
 
   &:hover {
     transform: scale(1.1);
     transition: .3s;
+  }
+
+  button {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    background-color: transparent;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    z-index: 50;
+    cursor: pointer;
+
+    .icon {
+      color: white;
+    }
   }
 
   picture {
